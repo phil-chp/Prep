e64680c5b9e3cbba3909776432d1f7810b507699
Mathis Vinay <mathis.vinay@gmail.com>
2022-05-17 15:36:28 +0200

    Updated prep for working only with commands

--- prep.sh
+++ prep.sh
@@ -1,124 +1,110 @@
-##
-## EPITECH PROJECT, 2021
-## Prep
-## File description:
-## File cleaner, coding style checker, error handler script
-## for epitech students.
-## Feel free to edit this file to your liking.
-## All credits go to the creators of the following programs
-## Made by Philippe Cheype - Epitech Nice - PGE2025
-##
-## Utilites needed:
-##     NormEZ   - https://github.com/ronanboiteau/NormEZ/
-##     bubulle  - https://github.com/aureliancnx/Bubulle-Norminette/
-##     cppcheck - http://cppcheck.sourceforge.net/
-##     deheader - https://gitlab.com/esr/deheader/
-##
-## Add this file with an alias to your terminal config for ease of use:
-##     alias €prep="[path to this file]"
-##
-## Last updated: 03/06/21
-##
+#!/usr/bin/env bash
 
-#######################################
-NORMEZ="$HOME/path/to/file/NormEZ.rb"
-DEHEADER="$HOME/path/to/file/deheader"
-#######################################
+# Prep
+#
+# File cleaner, coding style checker, error handler script
+# for Epitech students.
+# Feel free to edit this file to your liking.
+# All credits go to the creators of the following programs
+# Made by Philippe Cheype - Epitech Nice - PGE2025
+#
+# Utilites needed:
+#     NormEZ   - https://github.com/ronanboiteau/NormEZ/
+#     bubulle  - https://github.com/aureliancnx/Bubulle-Norminette/
+#     cppcheck - http://cppcheck.sourceforge.net/
+#     deheader - https://gitlab.com/esr/deheader/
+#
+# Github page: https://github.com/Philippe-cheype/Prep
+#
 
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]
+then
+  echo -e "prep [-h] [-f]
+A collection of useful tools for working with Epitech-like projects.
 
+USAGE:
+\t-h --help\tDisplay this help message
+\t-f --force\tForce prep execution even if the working directory doesn't contain any Makefile."
+  exit
+fi
+
+if [ "$1" == "-f" ] || [ "$1" == "--force" ]
+then
+  make fclean -s
+else
+  make fclean -s || { echo -e "Either you are not in a repo, or you are missing a Makefile\nStopping execution"; exit 1; }
+fi
 
 clear
-if [ "$1" = "-f" ];
-then
-    make fclean -s
-else
-    make fclean -s || { echo "\nEither you are not in a repo, or you are missing a Makefile\nStopping execution"; exit 1; }
-fi
 echo "Make fclean + Removing unnecessary files:"
 echo "- Make fclean done"
-find -name "*.o"      -delete && echo "- Removed .o files"
-find -name "*.gc*"    -delete && echo "- Removed criterion temp files"
-find -name "*.*~"     -delete && echo "- Removed emacs temp files"
-find -name "*~"       -delete && echo "- Removed emacs buffer files"
-find -name "#*#"      -delete && echo "- Removed emacs backup files"
-find -name ".#*"      -delete && echo "- Removed even more emacs backup files"
-find -name "*.vgcore" -delete && echo "- Removed valgrind log files"
-find -name "*.out"    -delete && echo "- Removed random bin files"
-find -name "*.tmp"    -delete && echo "- Removed temporary files made by vscode"
-echo "\nRemoved temp files.\nPress enter to continue..."
-read a
-
-
-
-clear
-ruby -v >/dev/null 2>&1
-if [ $? -eq 0 ]
-then
-    ruby $NORMEZ -h >/dev/null 2>&1
-    if [ $? -eq 0 ]
-    then
-        ruby $NORMEZ
-        echo "\nNormEZ done."
-    else
-        echo "NormEZ is either not installed, corrupted or the path to the file is wrong."
-        echo "If you have not set the correct path check step 3 of installation on the github page:"
-        echo "> https://github.com/Philippe-cheype/Prep#3-setting-the-correct-paths"
-        echo "Or download the files if you have not done it yet:"
-        echo "> https://github.com/ronanboiteau/NormEZ/"
-    fi
-else
-    echo "Ruby is either not installed or corrupted."
-    echo "> https://www.ruby-lang.org/en/documentation/installation/"
-fi
-
-echo "\nPress enter to continue..."
-read a
-
+find . -name "*.o"         -delete && echo "- Removed .o files"
+find . -name "*.gc*"       -delete && echo "- Removed criterion temp files"
+find . -name "*.*~"        -delete && echo "- Removed emacs temp files"
+find . -name "*~"          -delete && echo "- Removed emacs buffer files"
+find . -name "#*#"         -delete && echo "- Removed emacs backup files"
+find . -name ".#*"         -delete && echo "- Removed even more emacs backup files"
+find . -name "*.vgcore"    -delete && echo "- Removed valgrind log files"
+find . -name "*.out"       -delete && echo "- Removed random bin files"
+find . -name "*.tmp"       -delete && echo "- Removed temporary files made by vscode"
+find . -name "__pycache__" -delete && echo "- Removed python cache"
+find . -name "*.hi"        -delete && echo "- Removed haskell interface files"
+echo -e "\nRemoved temp files.\nPress enter to continue..."
+read -r a
 
 
 clear
-bubulle -v >/dev/null 2>&1
-if [ $? -eq 0 ]
+if type normez
 then
-    bubulle -ic -i g5,l2,o4,f002,l3
-    echo "\nBubulle done."
+  normez
+  echo -e "\nNormEZ done."
 else
-    echo "Bubulle is either not installed or corrupted."
-    echo "> https://github.com/aureliancnx/Bubulle-Norminette/"
+  echo "NormEZ wasn't found. Please re-install Prep or install NormEZ manually."
+  echo "- Prep: https://github.com/Philippe-cheype/Prep"
+  echo "- NormEZ: https://github.com/ronanboiteau/NormEZ/"
 fi
-echo "\nPress enter to continue..."
-read a
-
+echo -e "\nPress enter to continue..."
+read -r a
 
 
 clear
-cppcheck -h >/dev/null 2>&1
-if [ $? -eq 0 ]
+if type bubulle
 then
-    cppcheck -q .
-    echo "\nCppcheck done."
+  bubulle
+  echo -e "\nBubulle done."
 else
-    echo "CppCheck is either not installed or corrupted."
-    echo "> http://cppcheck.sourceforge.net/"
+  echo "Bubulle wasn't found. Please re-install Prep or install Bubulle manually."
+  echo "- Prep: https://github.com/Philippe-cheype/Prep"
+  echo "- Bubulle: https://github.com/aureliancnx/Bubulle-Norminette/"
 fi
-echo "\nPress enter to continue..."
-read a
-
+echo -e "\nPress enter to continue..."
+read -r a
 
 
 clear
-$DEHEADER -V >/dev/null 2>&1
-if [ $? -eq 0 ]
+if type cppcheck
 then
-    $DEHEADER -m "make all"
-    echo "\nDeheader done."
+  cppcheck -q .
+  echo -e "\nCppcheck done."
 else
-    echo "Deheader is either not installed, corrupted or the path to the file is wrong."
-        echo "If you have not set the correct path check step 3 of installation on the github page:"
-        echo "> https://github.com/Philippe-cheype/Prep#3-setting-the-correct-paths"
-        echo "Or download the files if you have not done it yet:"
-    echo "> https://gitlab.com/esr/deheader/"
+  echo "cppcheck wasn't found. Please re-install Prep or install cppcheck manually."
+  echo "- Prep: https://github.com/Philippe-cheype/Prep"
+  echo "- cppcheck: http://cppcheck.sourceforge.net/"
 fi
-echo "\nPrep finished.\nPress enter to exit..."
-read a
+echo -e "\nPress enter to continue..."
+read -r a
+
+
+clear
+if type deheader
+then
+  deheader
+  echo -e "\nDeheader done."
+else
+  echo "deheader wasn't found. Please re-install Prep or install deheader manually."
+  echo "- Prep: https://github.com/Philippe-cheype/Prep"
+  echo "- deheader: https://gitlab.com/esr/deheader/"
+fi
+echo -e "\nPrep finished.\nPress enter to exit..."
+read -r a
 clear
